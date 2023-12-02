df427d5d4567bf573bf2a08c0789d5f7d400f352
Saulo Ferraz <ferrazsaulo1@gmail.com>
2023-12-02 01:58:20 -0300

    Classe Cliente e Afins

---
+++ src/main/java/com/example/controller/ClienteController.java
@@ -0,0 +1,44 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.service.ClienteService;
+import com.example.model.Cliente;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return clienteService.getAllClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        return clienteService.getClienteById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Cliente saveCliente(@RequestBody Cliente cliente) {
+        return clienteService.saveCliente(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+        clienteService.deleteCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/example/service/ClienteService.java
@@ -0,0 +1,36 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.repository.ClienteRepository;
+import com.example.model.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> getClienteById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente saveCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void deleteCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
